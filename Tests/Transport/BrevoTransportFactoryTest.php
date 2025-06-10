ff6f96d987ad54db8e1779c693e91b1489dac90d
coop <78235646+coopcomputing@users.noreply.github.com>
2025-06-10 12:14:20 -0400

    Revert "update from main code"

--- Tests/Transport/BrevoTransportFactoryTest.php
+++ Tests/Transport/BrevoTransportFactoryTest.php
@@ -16,15 +16,12 @@ use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransport;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoSmtpTransport;
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
-use Symfony\Component\Mailer\Test\AbstractTransportFactoryTestCase;
-use Symfony\Component\Mailer\Test\IncompleteDsnTestTrait;
+use Symfony\Component\Mailer\Test\TransportFactoryTestCase;
 use Symfony\Component\Mailer\Transport\Dsn;
 use Symfony\Component\Mailer\Transport\TransportFactoryInterface;
 
-class BrevoTransportFactoryTest extends AbstractTransportFactoryTestCase
+class BrevoTransportFactoryTest extends TransportFactoryTestCase
 {
-    use IncompleteDsnTestTrait;
-
     public function getFactory(): TransportFactoryInterface
     {
         return new BrevoTransportFactory(null, new MockHttpClient(), new NullLogger());
